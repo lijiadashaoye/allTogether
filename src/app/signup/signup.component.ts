0fd624ed3b1ceda33927d23c10139b0d10bf8f60
lipeng <937681195@qq.com>
2018-08-21 09:45:15 +0800

    f

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,138 +1,157 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import {
   FormGroup,
   FormBuilder,
   Validators,
   FormControl
-} from '@angular/forms';
-import {
-  Router,
-  ActivatedRoute
-} from '@angular/router';
-import {
-  UserService
-} from '../user.service'
+} from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+import { UserService } from "../user.service";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"]
 })
 export class SignupComponent implements OnInit {
-  tipText = ''; // 提示
+  tipText = ""; // 提示
   resolveDatas; // 通过resolve获取的数据
-  buttonText = 'Signup';
+  buttonText = "Signup";
   constructor(
     public fb: FormBuilder,
     public route: Router,
     public userService: UserService,
-    private activeRoute: ActivatedRoute,
+    private activeRoute: ActivatedRoute
   ) {}
   forms: FormGroup;
   ngOnInit() {
     this.activeRoute.data // 获取resolve数据
       .subscribe(data => {
         if (data.resolveData.length > 0) {
-          this.resolveDatas = data.resolveData[0]
+          this.resolveDatas = data.resolveData[0];
         }
       });
 
     this.forms = this.fb.group({
-      name: ['', Validators.compose([Validators.required])],
-      password: ['', Validators.compose([Validators.required, this.validPassword])],
-      email: ['', Validators.compose([Validators.required, this.validEmail])],
-      job: ['', Validators.compose([Validators.required])],
-      logo: ['']
-    })
+      name: ["", Validators.compose([Validators.required])],
+      password: [
+        "",
+        Validators.compose([Validators.required, this.validPassword])
+      ],
+      email: ["", Validators.compose([Validators.required, this.validEmail])],
+      job: ["", Validators.compose([Validators.required])],
+      logo: [""]
+    });
     if (this.resolveDatas) {
       this.forms.patchValue({
         name: this.resolveDatas.name,
         password: this.resolveDatas.password,
         email: this.resolveDatas.email,
         job: this.resolveDatas.job,
-        logo: this.resolveDatas.logo,
-      })
-      this.buttonText = 'ChangeInformation'
+        logo: this.resolveDatas.logo
+      });
+      this.buttonText = "ChangeInformation";
     } else {
-      let num = Math.floor(Math.random() * 10)
+      let num = Math.floor(Math.random() * 10);
       let logoSrc = `assets/logo/${num}.jpg`;
       this.forms.patchValue({
         logo: logoSrc
-      })
-    };
+      });
+    }
   }
-  signup() { // 登陆行为
-    this.tipText = '';
+  signup() {
+    // 登陆行为
+    this.tipText = "";
     let kk = this.forms.controls;
     for (let i in kk) {
       if (kk[i].errors) {
         if (kk[i].errors.error && !kk[i].errors.required) {
-          this.tipText += kk[i].errors.error + '**'
-        } else
-        if (kk[i].errors.required) {
-          this.tipText += i + '未填写**'
+          this.tipText += kk[i].errors.error + "**";
+        } else if (kk[i].errors.required) {
+          this.tipText += i + "未填写**";
         }
       }
     }
-    if (this.forms.valid) {
+    if (this.forms.valid && this.forms.dirty) {
       if (this.resolveDatas) {
         let data = this.forms.value;
-        data['id'] = this.resolveDatas.id
+        data["id"] = this.resolveDatas.id;
         this.userService.changeUserInfo(data).subscribe(val => {
           this.resolveDatas = val;
-          this.tipText = '信息修改成功！'
-          setTimeout(_ => { // 信息提交成功后初始化组件
-            this.tipText = '';
-            this.forms.patchValue({ // this.forms.reset() 不好使
-              name: '',
-              password: '',
-              email: '',
-              job: '',
-              logo: '',
-            });
-            this.buttonText = 'Signup';
-          }, 2000)
-        })
+          this.tipText = "信息修改成功！";
+
+          // setTimeout(_ => {
+          //   // 信息提交成功后初始化组件(清空已填入的数据)
+          //   this.tipText = "";
+          //   this.forms.patchValue({
+          //     // this.forms.reset() 不好使
+          //     name: "",
+          //     password: "",
+          //     email: "",
+          //     job: "",
+          //     logo: ""
+          //   });
+          //   this.buttonText = "Signup";
+          // }, 2000);
+          setTimeout(_ => {
+            // 信息提交成功后初始化组件(不清空数据，只是变更其他验证状态)
+            this.tipText = "";
+            let controls = this.forms.controls;
+            for (let i in controls) {
+              let value = controls[i].value;
+              controls[i].markAsUntouched({ onlySelf: true }); // 将当前控件标记为untouched
+              controls[i].reset({ onlySelf: true }); // 重置当前input
+              controls[i].setValue(value); // 重置后再赋值，进行回显
+            }
+          }, 2000);
+        });
       } else {
-        this.userService.signup(this.forms.value)
-          .subscribe(val => {
-            this.route.navigate(['login'])
-          })
+        this.userService.signup(this.forms.value).subscribe(val => {
+          this.route.navigate(["login"]);
+        });
       }
+    } else {
+      this.tipText = "没有变更的信息！";
+      setTimeout(_ => {
+        // 信息提交成功后初始化组件
+        this.tipText = "";
+      }, 2000);
     }
   }
-  validEmail(c: FormControl) { // 邮箱验证
+  validEmail(c: FormControl) {
+    // 邮箱验证
     let reg = /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/;
     if (reg.test(c.value)) {
-      return null
+      return null;
     } else {
       return {
-        'error': '邮箱错误！'
-      }
+        error: "邮箱错误！"
+      };
     }
   }
-  validPassword(c: FormControl) { // 密码验证
+  validPassword(c: FormControl) {
+    // 密码验证
     if (c.value.length > 10 || c.value.length < 3) {
       if (c.value.length > 10) {
         return {
-          'error': '密码太长了！'
-        }
-      } else
-      if (c.value.length < 3) {
+          error: "密码太长了！"
+        };
+      } else if (c.value.length < 3) {
         return {
-          'error': '密码太短了！'
-        }
+          error: "密码太短了！"
+        };
       }
     } else {
-      return null
+      return null;
     }
   }
-  back() { //  退到登陆页，并传递数据
-    this.route.navigate(['login', {
-      ...this.resolveDatas
-    }])
+  back() {
+    //  退到登陆页，并传递数据
+    this.route.navigate([
+      "login",
+      {
+        ...this.resolveDatas
+      }
+    ]);
   }
 }
