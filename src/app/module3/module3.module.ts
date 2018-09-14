247624f113e89b4bae41784f73ad76b71aeaeba6
lipeng <937681195@qq.com>
2018-09-14 14:03:43 +0800

    f

--- src/app/module3/module3.module.ts
+++ src/app/module3/module3.module.ts
@@ -1,34 +1,22 @@
-import {
-  NgModule
-} from '@angular/core';
-import {
-  CommonModule
-} from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import {
-  Module3Component
-} from './module3.component';
-import { SomeCss3Component } from './some-css3/some-css3.component';
-import {FormsModule} from '@angular/forms'
+import { Module3Component } from "./module3.component";
+import { SomeCss3Component } from "./some-css3/some-css3.component";
+import { FormsModule } from "@angular/forms";
 
-import {
-  module3RoutingModule
-} from './module3.route';
-import { JsLearnComponent } from './js-learn/js-learn.component';
-import { ZaxiangComponent } from './zaxiang/zaxiang.component';
+import { module3RoutingModule } from "./module3.route";
+import { JsLearnComponent } from "./js-learn/js-learn.component";
+import { ZaxiangComponent } from "./zaxiang/zaxiang.component";
+
 
 @NgModule({
-  imports: [
-    CommonModule,
-    module3RoutingModule,
-    FormsModule
-  ],
+  imports: [CommonModule, module3RoutingModule, FormsModule],
   declarations: [
     Module3Component,
     SomeCss3Component,
     JsLearnComponent,
-    ZaxiangComponent,
-
+    ZaxiangComponent
   ]
 })
 export class Module3Module {}
