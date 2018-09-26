7159e984b30646614552fc4482cb659a3b902f9e
lipeng <937681195@qq.com>
2018-09-26 10:19:23 +0800

    f

--- src/app/module3/js-learn/transform/transform.component.ts
+++ src/app/module3/js-learn/transform/transform.component.ts
@@ -39,4 +39,58 @@ export class TransformComponent implements OnInit {
   // y 垂直位移
 
   // canvas的transform的rotate方法是默认绕着canvas的原点（即左上角）旋转
+
+  fasdf() {
+    // 原始transform计算方法
+    function Matrix() {
+      var x =
+        arguments.length > 0
+          ? Array.prototype.slice.call(arguments)
+          : [1, 0, 0, 1, 0, 0];
+      for (var p in x) {
+        this[p] = x[p];
+      }
+      this.length = x.length;
+    }
+    Matrix.prototype = {
+      rotate: function(r) {
+        var cos = Math.cos(r),
+          sin = Math.sin(r),
+          mx = this,
+          a = mx[0] * cos + mx[2] * sin,
+          b = mx[1] * cos + mx[3] * sin,
+          c = -mx[0] * sin + mx[2] * cos,
+          d = -mx[1] * sin + mx[3] * cos;
+        this[0] = a;
+        this[1] = b;
+        this[2] = c;
+        this[3] = d;
+        return this;
+      },
+      skew: function(x, y) {
+        var tanX = Math.tan(x),
+          tanY = Math.tan(y),
+          mx0 = this[0],
+          mx1 = this[1];
+        this[0] += tanY * this[2];
+        this[1] += tanY * this[3];
+        this[2] += tanX * mx0;
+        this[3] += tanX * mx1;
+        return this;
+      },
+      translate: function(x, y) {
+        this[4] += this[0] * x + this[2] * y;
+        this[5] += this[1] * x + this[3] * y;
+        return this;
+      },
+      scale: function(x, y) {
+        var mx = this;
+        this[0] *= x;
+        this[1] *= x;
+        this[2] *= y;
+        this[3] *= y;
+        return this;
+      }
+    };
+  }
 }
