003ac169921ea275d809bd2a4ed4545f60a0984e
lipeng <937681195@qq.com>
2018-10-17 14:34:05 +0800

    f

--- src/app/module3/some-css3/some-css3.component.ts
+++ src/app/module3/some-css3/some-css3.component.ts
@@ -1,7 +1,4 @@
-import {
-  Component,
-  OnInit
-} from "@angular/core";
+import { Component, OnInit, Renderer2 } from "@angular/core";
 
 @Component({
   selector: "app-some-css3",
@@ -9,26 +6,58 @@ import {
   styleUrls: ["./some-css3.component.css"]
 })
 export class SomeCss3Component implements OnInit {
-  listData = [{
-    name: "首页",
-    icon: "icon-zhibiao"
-  }, {
-    name: "指标",
-    icon: "icon-home"
-  }]
-  constructor() {}
+  listData = [
+    {
+      name: "首页",
+      icon: "icon-zhibiao"
+    },
+    {
+      name: "指标",
+      icon: "icon-home"
+    }
+  ];
+  constructor(private rd: Renderer2) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    document.ondragover = function(e) {
+      // 防止图片在新窗口打开
+      e.preventDefault();
+    };
+  }
   setClass(icon) {
     return icon;
   }
 
   // 元素的css样式增删
   hide(ev) {
-    ev['classList'].remove('active');
+    ev["classList"].remove("active");
   }
 
   show(ev) {
-    ev['classList'].add('active');
+    ev["classList"].add("active");
+  }
+
+  /********************************************** */
+  itemX = null;
+  itemY = null;
+  dragstartFn(e) {
+    let tar = e.target;
+    this.itemX = e.clientX - tar.offsetLeft;
+    this.itemY = e.clientY - tar.offsetTop;
+  }
+  dragFn(e: Event) {
+    let tar = e.target;
+    let mouseX = e["clientX"];
+    let mouseY = e["clientY"] + 2;
+    this.rd.setAttribute(
+      tar,
+      "style",
+      `position:absolute;left:${mouseX - this.itemX}px;top:${mouseY -
+        this.itemY}px`
+    );
+  }
+  dragendFn(e: Event) {
+    e.preventDefault();
+    e.stopPropagation();
   }
 }
