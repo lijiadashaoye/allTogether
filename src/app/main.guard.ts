191a2c281e83f12d56ce042b06200838e9a6af33
lipeng <lipeng@bicai365.com>
2018-12-05 16:40:47 +0800

    d

--- src/app/main.guard.ts
+++ src/app/main.guard.ts
@@ -15,6 +15,16 @@ import {
   UserService
 } from './main-http-service/user.service'
 /*******************************************************************/
+// 注册在 NgModule 下的 service 都无法被 tree-shaking 掉
+// service 是组册在根 NgModules 下
+// @Injectable({
+//   providedIn: 'root' 
+// })
+// 新的设定方法可以被 tree-shaking 掉了，这对于最后产生出來的 bundle 大小，会有很大的帮助
+// service 是注册在某一個 NgModules 下
+// @Injectable({
+//   providedIn: HeroModule
+// })
 // resolve方法会在路由开始，页面还未加载时，提前进行一些行为，但如果是执行http，会阻碍
 // 页面的加载，出现一点时间的空白
 @Injectable() // 用来定义怎么resolve数据的服务
